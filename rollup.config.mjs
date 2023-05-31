01fc83f75251d252a0305fc635ad284fdba51696
typhonrt <support@typhonjs.io>
2023-05-31 15:43:47 -0700

    remove unused plugins

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,3 @@
-import commonjs            from '@rollup/plugin-commonjs';
-import resolve             from '@rollup/plugin-node-resolve';
 import typescript          from '@rollup/plugin-typescript';
 import dts                 from 'rollup-plugin-dts';
 
