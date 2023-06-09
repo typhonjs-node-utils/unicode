0e85b33ae696fe60d30a424e81541cf563ee9951
typhonrt <support@typhonjs.io>
2023-06-09 16:15:06 -0700

    update build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,13 +1,29 @@
-import typescript          from '@rollup/plugin-typescript';
-import dts                 from 'rollup-plugin-dts';
+import replace    from '@rollup/plugin-replace';
+import typescript from '@rollup/plugin-typescript';
+import dts        from 'rollup-plugin-dts';
 
 // Produce sourcemaps or not.
 const sourcemap = true;
 
+/**
+ * @type {import('@rollup/plugin-replace').RollupReplaceOptions}
+ */
+const replaceOptions = {
+   values: {
+      '#runtime/data/compress': 'fflate',
+      '#runtime/data/format/base64': 'js-base64',
+   },
+   preventAssignment: true,
+   delimiters: ['', '']
+};
+
+const externalTRL = [/#runtime\/*/g];
+
 export default () =>
 {
    return [
-      {   // This bundle is for the Node distribution.
+      // Main Distribution Bundles -----------------------------------------------------------------------------------
+      {
          input: 'src/grapheme/index.ts',
          output: [{
             file: './dist/index.js',
@@ -16,13 +32,67 @@ export default () =>
             sourcemap,
          }],
          plugins: [
+            replace(replaceOptions),
+            typescript({ include: ['src/**/*'] })
+         ]
+      },
+      {
+         input: 'src/unicode-trie/index.ts',
+         output: [{
+            file: './dist/unicode-trie/index.js',
+            format: 'es',
+            generatedCode: { constBindings: true },
+            sourcemap,
+         }],
+         plugins: [
+            replace(replaceOptions),
+            typescript({ include: ['src/**/*'] })
+         ]
+      },
+
+      // Main TS Declaration Bundles ---------------------------------------------------------------------------------
+      {
+         input: 'src/grapheme/index.ts',
+         output: [{
+            file: `./dist/index.d.ts`,
+            format: 'es',
+            sourcemap: false
+         }],
+         plugins: [
+            replace(replaceOptions),
+            dts()
+         ]
+      },
+      {
+         input: 'src/unicode-trie/index.ts',
+         output: [{
+            file: `./dist/unicode-trie/index.d.ts`,
+            format: 'es',
+            sourcemap: false
+         }],
+         plugins: [
+            replace(replaceOptions),
+            dts()
+         ]
+      },
+
+      // TRL Distribution Bundles ------------------------------------------------------------------------------------
+      {
+         input: 'src/grapheme/index.ts',
+         output: [{
+            file: './dist-trl/index.js',
+            format: 'es',
+            generatedCode: { constBindings: true },
+            sourcemap,
+         }],
+         plugins: [
             typescript({ include: ['src/**/*'] })
          ]
       },
       {   // This bundle is for the Node distribution.
          input: 'src/unicode-trie/index.ts',
          output: [{
-            file: './dist/unicode-trie/index.js',
+            file: './dist-trl/unicode-trie/index.js',
             format: 'es',
             generatedCode: { constBindings: true },
             sourcemap,
@@ -32,27 +102,26 @@ export default () =>
          ]
       },
 
-      {   // This bundle is for bundled types.
+      // TRL TS Declaration Bundles ----------------------------------------------------------------------------------
+      {
          input: 'src/grapheme/index.ts',
          output: [{
-            file: `./dist/index.d.mts`,
+            file: `./dist-trl/index.d.ts`,
             format: 'es',
             sourcemap: false
          }],
          plugins: [
-            typescript({ include: ['src/**/*'], sourceMap: false, inlineSources: false }),
             dts()
          ]
       },
       {   // This bundle is for bundled types.
          input: 'src/unicode-trie/index.ts',
          output: [{
-            file: `./dist/unicode-trie/index.d.mts`,
+            file: `./dist-trl/unicode-trie/index.d.ts`,
             format: 'es',
             sourcemap: false
          }],
          plugins: [
-            typescript({ include: ['src/**/*'], sourceMap: false, inlineSources: false }),
             dts()
          ]
       },
